1584761cbbfc64c8e0c551b003ecc4806ff5c4ec
Abraham Egnor <abe.egnor@gmail.com>
2020-08-16 09:12:58 -0400

    errors

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,43 @@
 use std::collections::{HashMap, HashSet};
+use std::error::Error;
+use std::fmt;
 
 use reqwest::{self, StatusCode};
 use serde::{Deserialize};
 use serde::de::DeserializeOwned;
-use serde_json::Value;
+//use serde_json::Value;
 
 static KEY: &str = "9E827119-71EE-774C-88AE-B4D1D4B30868205CD293-9A69-415C-A3E3-4CB1E184722C";
 
-fn fetch<T: DeserializeOwned>(client: &reqwest::blocking::Client, path: &str) -> T {
-    let res = client.get(&format!("https://api.guildwars2.com/v2/{}?access_token={}", path, KEY))
-        .send().unwrap();
-    //println!("{:?}", res);
-    if res.status() != StatusCode::OK {
-        panic!("{:?}", res);
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+struct Failed(String);
+
+impl fmt::Display for Failed {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Failed({})", self.0)
     }
-    res.json().unwrap()
 }
 
-fn fetch_unauth<T: DeserializeOwned>(client: &reqwest::blocking::Client, path: &str) -> T {
-    let res = client.get(&format!("https://api.guildwars2.com/v2/{}", path))
-        .send().unwrap();
+impl Error for Failed {}
+
+macro_rules! failed {
+    ($($arg:expr),*) => {
+        return Err(Box::new(Failed(format!($($arg),*))))
+    };
+}
+
+fn fetch<T: DeserializeOwned>(
+    client: &reqwest::blocking::Client,
+    path: &str,
+) -> Result<T> {
+    let res = client.get(&format!("https://api.guildwars2.com/v2/{}?access_token={}", path, KEY)).send()?;
+    //println!("{:?}", res);
     if res.status() != StatusCode::OK {
-        panic!("{:?}", res);
+        failed!("{:?}", res)
     }
-    res.json().unwrap()
+    Ok(res.json()?)
 }
 
 #[derive(Debug, Clone, Deserialize)]
@@ -52,14 +66,14 @@ struct Recipe {
     chat_link: String,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let client = reqwest::blocking::Client::new();
-    let names: Vec<String> = fetch(&client, "characters");
+    let names: Vec<String> = fetch(&client, "characters")?;
     println!("{:?}", names);
     let mut ids_by_char = HashMap::<&str, Vec<i32>>::new();
     let mut all_ids = HashSet::<i32>::new();
     for name in &names {
-        let mut r: Recipes = fetch(&client, &format!("characters/{}/recipes", name));
+        let mut r: Recipes = fetch(&client, &format!("characters/{}/recipes", name))?;
         println!("{}: {}", name, r.recipes.len());
         for id in &r.recipes {
             all_ids.insert(*id);
@@ -67,4 +81,5 @@ fn main() {
         ids_by_char.entry(name).or_insert(vec![]).append(&mut r.recipes);
     }
     println!("{}", all_ids.len());
+    Ok(())
 }
